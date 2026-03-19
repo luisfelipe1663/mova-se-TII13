b63a3f7c399c543b587aeb321b8e0ce5f1e65d19
luisfelipe1663 <luisfelipe.10042008@gmail.com>
2026-03-19 16:23:25 -0300

    subindo o repositorio

---
+++ mova_se/js/interacao.js
@@ -0,0 +1,14 @@
+//carrossel
+$(document).ready(function(){
+    $('#destaque').slick({
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+
+    });
+});
+//modal
+
+
+
